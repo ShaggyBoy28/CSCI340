3f78f94e532e5525cec25f77d127a7135549d24e
ShaggyBoy28 <70717301+ShaggyBoy28@users.noreply.github.com>
2020-09-15 13:36:01 -0500

    Update rgi.h

--- rgi.h
+++ rgi.h
@@ -1,9 +1,15 @@
 #ifndef H_RGI // should not be defined any place else 
 #define H_RGI // same const value as for ifndef directive
 
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
 // Add needed header files here ..
-void genRndNums(vector<int> &v)
-void printVec(const vector<int> &v)
+void genRndNums(vector<int> &);
+void printVec(const vector<int> &);
 // You need to implement these two functions ...
 // void genRndNums(vector<int> &v);
 // void printVec(const vector<int> &v);
